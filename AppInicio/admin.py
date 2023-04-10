3e597f47cf754f00364a50b9ec8cb30c40596fcd
Mauricio Castro <mauriciocas2013.mc@gmail.com>
2023-04-09 22:48:10 -0300

    Creo la clase definitiva y la registro, agrego superusuario de admin

--- AppInicio/admin.py
+++ AppInicio/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from AppInicio.models import Tarea
 
 # Register your models here.
+
+admin.site.register(Tarea)
--- AppInicio/models.py
+++ AppInicio/models.py
@@ -1,24 +1,21 @@
 from django.db import models
+from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
 
-    
-class Usuario(models.Model):
-    
-    nombre = models.CharField(max_length=20)
-    apellido = models.CharField(max_length=20)
-    edad = models.IntegerField()
-    
+class Tarea(models.Model):
+    usuario = models.ForeignKey(User,
+                                on_delete=models.CASCADE,
+                                null=True,
+                                blank=True)
+    titulo = models.CharField(max_length=200)
+    descripcion = models.TextField(null=True,
+                                   blank=True)
+    completo = models.BooleanField(default=False)
+    creado = models.DateTimeField(auto_now_add=True)
+
     def __str__(self):
-        return f"{self.nombre} {self.apellido} "
-    
+        return self.titulo
 
-class Profesional(models.Model):
-    
-    nombre = models.CharField(max_length=20)
-    apellido = models.CharField(max_length=20)
-    edad = models.IntegerField()
-    profesion = models.CharField(max_length=30)
-    
-    def __str__(self):
-        return f"{self.nombre} {self.apellido} "
+    class Meta:
+        ordering = ['completo']
