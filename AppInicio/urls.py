7376cc344b5bc7dccf4b92e6b2edbd1857ed284f
Mauricio Castro <mauriciocas2013.mc@gmail.com>
2023-04-10 18:14:19 -0300

    Se trabaja en templates necesarios para eliminar, crear y editar tareas, ademas se trabaja en forma de logueo y deslogueo, falta retriccion de accesos a usuarios y seguir dandole formato al administrador de tareas

--- AppInicio/urls.py
+++ AppInicio/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path
-from AppInicio.views import primer_template,mostrar_m,Inicio,ListaPendientes,DetalleTarea,CrearTarea
-
+from AppInicio.views import primer_template,mostrar_m,Inicio,ListaPendientes,DetalleTarea,CrearTarea,EditarTarea,EliminarTarea,Logueo
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('',Inicio, name="inicio"),
+    path('login',Logueo.as_view(),name="login"),
+    path('logout',LogoutView.as_view(next_page="login"),name="logout"),
     path('tem/',primer_template, name="tem"),
     path('m/',mostrar_m,name="m"),
     path('pendientes/',ListaPendientes.as_view(),name="pendientes"),
     path('tarea/<int:pk>',DetalleTarea.as_view(),name="tarea"),
     path('crear-tarea/',CrearTarea.as_view(),name="crear-tarea"),
+    path('ediar-tarea/<int:pk>',EditarTarea.as_view(),name="editar-tarea"),
+    path('eliminar-tarea/<int:pk>',EliminarTarea.as_view(),name="eliminar-tarea"),
 ]
--- AppInicio/views.py
+++ AppInicio/views.py
@@ -4,11 +4,21 @@ from AppInicio.models import Tarea
 from django.views.generic.list import ListView
 from django.views.generic import TemplateView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView,UpdateView,DeleteView
 from django.urls import reverse_lazy
+from django.contrib.auth.views import LoginView
 
 
 # Create your views here.
+
+class Logueo(LoginView):
+    template_name = r"appinicio/login.html"
+    fields = '__all__'
+    redirect_authenticated_user = True
+    
+    def get_success_url(self):
+        return reverse_lazy("pendientes")
+
 
 def Inicio(request):
     
@@ -23,9 +33,9 @@ def primer_template(request):
 
 def mostrar_m(request):
     
-    datos = {"m":"es un lindo dia"}
+    # datos = {"m":"es un lindo dia"}
     
-    return render(request, r"appinicio/mostrar_m.html",datos)
+    return render(request, r"appinicio/mostrar_m.html")
 
 
 class ListaPendientes(ListView):
@@ -43,6 +53,23 @@ class CrearTarea(CreateView):
     model = Tarea
     template_name = "appinicio/crear_tarea.html"
     fields = '__all__'
-    success_url = reverse_lazy("AppInicio")
+    success_url = reverse_lazy("pendientes")
+    
+
+class EditarTarea(UpdateView):
+    model = Tarea
+    template_name = "appinicio/crear_tarea.html"
+    fields = '__all__'
+    success_url = reverse_lazy("pendientes")
+    
+    
+class EliminarTarea(DeleteView):
+    model = Tarea
+    template_name = "appinicio/eliminar_tarea.html"
+    context_object_name = "tarea"
+    success_url = reverse_lazy("pendientes")
+    
+    
+    
     
     
